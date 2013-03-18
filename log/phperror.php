6c35903d54b039364ce765bd05084deee5170077
axiuno <axiuno@gmail.com>
2013-03-18 10:46:48 +0800

    modified:   .gitignore 	modified:   log/.gitignore

---
+++ log/phperror.php
@@ -0,0 +1,45 @@
+<?php exit;?>	2013-3-11 16:48	127.0.0.1	/	MySQL Query Error:REPLACE INTO bbs_runtime SET k='cronlock',v='1',expiry='0'. Unknown column 'expiry' in 'field list' File: D:\www\xxx.net\xiunophp\db\db_mysql.class.php [339]	
+<?php exit;?>	2013-3-11 16:54	127.0.0.1	/	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\bbs_view_index.htm.php, Line: 56 File: D:\www\xxx.net\xiunophp\core\core.class.php [293]	
+<?php exit;?>	2013-3-14 22:04	127.0.0.1	/?attach-dialog-aid-3-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:05	127.0.0.1	/?attach-dialog-aid-3-ajax-1.htm	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:07	127.0.0.1	/?attach-dialog-aid-3-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:07	127.0.0.1	/?attach-dialog-aid-4-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:07	127.0.0.1	/?attach-dialog-aid-3-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:07	127.0.0.1	/?attach-dialog-aid-5-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:07	127.0.0.1	/?attach-dialog-aid-4-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:07	127.0.0.1	/?attach-dialog-aid-3-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:07	127.0.0.1	/?attach-dialog-aid-3-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:07	127.0.0.1	/?attach-dialog-aid-3-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:07	127.0.0.1	/?attach-dialog-aid-3-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:08	127.0.0.1	/?attach-dialog-aid-3-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:08	127.0.0.1	/?attach-dialog-aid-4-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:08	127.0.0.1	/?attach-dialog-aid-5-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:08	127.0.0.1	/?attach-dialog-aid-3-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-14 22:24	127.0.0.1	/?attach-dialog-aid-3-ajax-1.htm&ajax=1	Not found model:pay File: D:\www\xxx.net\xiunophp\core\base_control.class.php [34]	
+<?php exit;?>	2013-3-15 01:16	127.0.0.1	/?post-thread-fid-1-ajax-1.htm	[Warning] : include_once(D:/www/xxx.net/xiunophp/lib/html_safe.class.php) [<a href='function.include-once'>function.include-once</a>]: failed to open stream: No such file or directory in File D:\www\xxx.net\xiunophp\lib\xn_html_safe.class.php, Line: 1924 File: D:\www\xxx.net\xiunophp\core\core.class.php [293]	
+<?php exit;?>	2013-3-15 01:33	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:34	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:34	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:34	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:34	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:34	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:35	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:35	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:35	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:35	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:35	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:42	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:44	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:44	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:44	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:44	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:58	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:58	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:58	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:58	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:59	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:59	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:59	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 01:59	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 02:00	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
+<?php exit;?>	2013-3-15 02:00	127.0.0.1	/?pm-new-ajax-1.htm	[Warning] : Invalid argument supplied for foreach() in File D:\www\xxx.net\tmp\_runtime.php, Line: 11	
