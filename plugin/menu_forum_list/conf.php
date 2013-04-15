514e820f14f79f11c6c3b549e8fd1bb6b2d27af4
axiuno <axiuno@gmail.com>
2013-04-15 16:57:21 +0800

    modified:   control/index_control.class.php 	modified:   plugin/defend_flooding_posts/conf.php 	new file:   plugin/menu_forum_list/conf.php 	new file:   plugin/menu_forum_list/footer_end.htm 	new file:   plugin/menu_forum_list/header_menu_end.htm 	new file:   plugin/menu_forum_list/index_control_after.php 	modified:   view/common.css 	modified:   view/footer.htm 	modified:   view/header.htm

---
+++ plugin/menu_forum_list/conf.php
@@ -0,0 +1,9 @@
+<?php
+
+return 	array (
+	'name' => '导航栏版块列表',	// 插件名
+	'brief' => '每个版块调用若干篇主题。',
+	'version' => '1.0',		// 插件版本
+	'bbs_version'=>'2.0.2',		// 插件支持的 Xiuno BBS 版本
+);
+?>
---
+++ plugin/menu_forum_list/index_control_after.php
@@ -0,0 +1,28 @@
+	public function on_list() {
+		
+		$this->_checked['forum_list'] = ' class="checked"';
+		
+		$forumarr = $this->conf['forumarr'];
+		$threadlists = $this->runtime->get('threadlists');
+		if(empty($threadlists)) {
+			foreach($forumarr as $fid=>$name) {
+				if(!empty($forumarr[$fid])) {
+					$access = $this->forum_access->read($fid, $this->_user['groupid']);
+					if(!empty($access) && !$access['allowread']) {
+						unset($forumarr[$fid]);
+						continue;
+					}
+				}
+				$threadlist = $this->thread->get_threadlist_by_fid($fid, 0, 0, 0, 10, 0);
+				foreach($threadlist as &$thread) {
+					$thread['dateline_fmt'] = misc::minidate($thread['dateline']);
+					$thread['subject_fmt'] = utf8::substr($thread['subject'], 0, 24);
+				}
+				$threadlists[$fid] = $threadlist;
+			}
+			$this->runtime->set('threadlists', $threadlists, 60); // todo:一分钟的缓存时间！这里可以根据负载进行调节。
+		}
+		$this->view->assign('forumarr', $forumarr);
+		$this->view->assign('threadlists', $threadlists);
+		$this->view->display('index_list.htm');
+	}
