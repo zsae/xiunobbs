498080095b5e400852ce790d3f4065efd26c346a
xiuno <axiuno@gmail.com>
2013-04-22 20:54:55 +0800

    modified:   control/post_control.class.php

--- control/post_control.class.php
+++ control/post_control.class.php
@@ -124,7 +124,7 @@ class post_control extends common_control {
 				// hook post_thread_create_after.php
 				
 				$this->thread_views->create(array('tid'=>$tid, 'views'=>0));
-				if(!isset($this->conf['forumarr'][$fid])) {
+				if(isset($this->conf['forumarr'][$fid])) {
 					$this->thread_new->create(array('fid'=>$fid, 'tid'=>$tid, 'lastpost'=>$_SERVER['time']));
 				}
 				
