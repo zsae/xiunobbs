bab77f50e34a95485e13fe2593110df6ede73b33
axiuno <axiuno@gmail.com>
2013-04-24 10:51:41 +0800

    modified:   control/attach_control.class.php 	modified:   plugin/index_two_column/plugin_index_two_column.htm 	new file:   plugin/xn_thread_fire_new_icon/conf.php 	new file:   plugin/xn_thread_fire_new_icon/thread_list_line_inc_subject_after.htm 	modified:   view/thread_list_line.inc.htm

---
+++ plugin/xn_thread_fire_new_icon/conf.php
@@ -0,0 +1,9 @@
+<?php
+
+return 	array (
+	'name' => '主题列表 new fire 图标',			// 插件名
+	'brief' => '在主题列表后显示 new, fire icon，增强版块活跃气氛。',
+	'version' => '1.0.0',			// 插件版本
+	'bbs_version' => '2.0.3',		// 插件支持的 Xiuno BBS 版本
+);
+?>
